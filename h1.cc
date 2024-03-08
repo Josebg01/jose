c18afa3b32cd997f99031c16de1b2ab0ed2cfb6d
Josebg01 <159161383+Josebg01@users.noreply.github.com>
2024-03-08 18:02:26 -0500

    Update h1.cc

--- h1.cc
+++ h1.cc
@@ -6,7 +6,7 @@ int main(int cantidad, char* argumentos[]) {
     if (cantidad < 2) {
         cout << "Error: No hay argumentos" << endl;
     }
-    for ( int i=cantidad-1; i >= 0; --i) {
+    for ( int i=cantidad-1; i > 0; --i) {
         cout << argumentos[i] << endl;
     }
 
